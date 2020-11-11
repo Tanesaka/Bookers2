3662242a5b29615b5e1efb961f550c22406f7073
Tanesaka <seedtanesa@outlook.jp>
2020-11-11 11:08:34 +0000

    [3-10]Update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # 下記、合っているか不明　5章参照
+    @books = @user.books.reverse_order
+    @book = Book.find(params[:id])
+  end
+
+  #下記、 プロフィール画像入れるとき5章参照
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+end
